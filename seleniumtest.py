633e0e4cf636a3784482cbd2e5a8b7b5d30153a1
duvuru <68279444+duvuru@users.noreply.github.com>
2020-08-05 16:43:35 +0100

    Update seleniumtest.py

--- seleniumtest.py
+++ seleniumtest.py
@@ -6,11 +6,11 @@ display = Display(visible=0, size=(800, 600))
 display.start()
 
 browser = webdriver.Firefox()
-browser.get('http://192.168.33.20:8080/dev/')
+browser.get('http://192.168.33.20:8080/devenv/')
 print browser.title
 var1 = browser.title
 print var1
-if "Hello Java" in var1:
+if "Hello Java2" in var1:
     print "Test Successful"
 else:
     raise Exception("Test Failed")
